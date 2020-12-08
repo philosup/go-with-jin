efd3ff611700f75b688b34b1ab8ebb6c8d1ae331
philosup <philosup@gmail.com>
2020-12-08 23:12:15 +0900

    몽고디비 설치 및 연결 테스트 코드 추가.

--- README.md
+++ README.md
@@ -36,9 +36,30 @@ func main() {
 		c.JSON(200, gin.H{
 			"message": "pong",
 		})
-	})
+    })
+    
+    r.GET("/time", api.Time)
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
 
 ```
 > go run main.go
+
+http://localhost:8080/ping
+
+## MongoDB  
+
+### 설치 https://docs.mongodb.com/manual/tutorial/install-mongodb-on-windows/
+
+다운로드 
+https://www.mongodb.com/try/download/community?tck=docs_server
+
+MongoDB Community Server 4.4.2
+
+https://m.blog.naver.com/wideeyed/221815886721
+
+
+
+https://www.mongodb.com/golang
+
+> go get go.mongodb.org/mongo-driver
--- main.go
+++ main.go
@@ -1,6 +1,79 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func mongoConn() (client *mongo.Client) {
+	// credential := options.Credential{
+	//    Username: "<USER_NAME>",
+	//    Password: "<PASSWORD>",
+	// }
+	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017") //.SetAuth(credential)
+	client, err := mongo.Connect(context.TODO(), clientOptions)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Check the connection
+	err = client.Ping(context.TODO(), nil)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("MongoDB Connection Made")
+	return client
+}
+func initMongoDB() {
+	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://localhost:27017"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	err = client.Connect(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer client.Disconnect(ctx)
+}
+
+type Post struct {
+	Title string `json:"title,omitempty"`
+	Body  string `json:"body,omitempty"`
+}
+
+func InsertPost(title string, body string) {
+
+	client := mongoConn()
+
+	post := Post{title, body}
+	collection := client.Database("testdb").Collection("post")
+	insertResult, err := collection.InsertOne(context.TODO(), post)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Inserted post with ID:", insertResult.InsertedID)
+}
+
+// func GetPost(id bson.ObjectId) {
+// 	collection := client.Database("testdb").Collection("post")
+// 	filter := bson.D
+// 	var post Post
+
+// 	err := collection.FindOne(context.TODO(), filter).Decode(&post)
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+// 	fmt.Println("Found post with title ", post.Title)
+// }
 
 func main() {
 	r := gin.Default()
@@ -9,5 +82,8 @@ func main() {
 			"message": "pong",
 		})
 	})
+
+	InsertPost("test1", "body1")
+
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
